67dc0373d4e3db4acbdc86da718dd6e972177dde
unknown <simonber2001@gmail.com>
2024-03-28 11:38:51 +0100

    added command button for ticket creation

--- src/CommandList.js
+++ src/CommandList.js
@@ -9,6 +9,14 @@ function getCommandList(){
         .setName('ping')
         .setDescription('this is a ping command')
     )
+
+    commands.push(
+        new Discord.SlashCommandBuilder()
+        .setName('setup_ticket_channel')
+        .setDescription('initialise l\'outil \"créer un ticket\" dans ce salon')
+    )
+
+
     return commands
 }
 
--- src/interactionCreateManager.js
+++ src/interactionCreateManager.js
@@ -1,6 +1,42 @@
-function interactionCreate(interaction){
+const Discord = require('discord.js')
+
+
+
+async function setup_ticket_channel(interaction){
+
+    const exampleEmbed = new Discord.EmbedBuilder()
+        .setColor(0x623460)
+        .setTitle('Ouvrir un ticket')
+        .setDescription('En cliquant sur \"Créer un Ticket\", un ticket s\'ouvrira pour vous')
+        
+    const button = new Discord.ButtonBuilder()
+		.setCustomId('create Ticket')
+		.setLabel('Créer un ticket')
+		.setStyle(Discord.ButtonStyle.Primary);
+
+    const row = new Discord.ActionRowBuilder()
+			.addComponents(button);
+
+    interaction.deferReply();
+    interaction.deleteReply();
+
+    const channel = interaction.guild.channels.cache.get(interaction.channelId)
+    channel.send({
+        embeds: [exampleEmbed],
+        components: [row]
+    })
+
+}
+
+
+function interactionCreate(interaction, bot){
+    
     if (interaction.commandName === "ping"){
-        interaction.reply("pong")
+        setup_ticket_channel(interaction)
+    }
+
+    if (interaction.commandName === "setup_ticket_channel"){
+        setup_ticket_channel(interaction)
     }
 }
 
